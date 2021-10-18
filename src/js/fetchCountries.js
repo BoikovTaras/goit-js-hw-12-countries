74f2e5d25bfbea260b9ba20df0f40a3328482b2a
Taras Boikov <tarasgowest@gmail.com>
2021-10-19 01:07:47 +0300

    парсит разметку

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,22 @@
 export default class CountryInformation {
-    // constructor() { };
-
+    constructor() { };
     countryInformation() {
-        const url = 'https://restcountries.com/v2/name/peru';
-        fetch(url)
-        .then(r => r.json)
-        .then(console.log);
+        
      };
-    
-
 };
+
+import countryCard from '../templates/country-card.hbs';
+
+const url = 'https://restcountries.com/v2/name/peru';
+fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {
+        console.log(country);
+        const countryResult = countryCard(country);
+        console.log(countryResult);
+    })
+    .catch(error => {
+        console.log(error)
+    })
