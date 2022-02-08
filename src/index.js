f93a39807df3f136ff57b67456a8223a6c4c079e
Taras Boikov <tarasgowest@gmail.com>
2022-02-09 00:48:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,23 +19,19 @@ const inputArea = document.querySelector('.js-input-area')
 
 
 const fetchCountry = e => {
-    console.log(e.target.value);
     countryInformation(e.target.value)
     .then(response => {
         return response.json();
     })
     .then(country => {
         if (country.length === 1) {
-            console.log('country', country);
             let countryResult = countryCard(country);
             countryArea.innerHTML = countryResult;
         } else if (country.length >= 2 && country.length <= 10) {
-            console.log('country 2-10', country);
             countryArea.innerHTML = country
             .map(({ name }) => `<li>${name}</li>`)
             .join('');
         } else if (country.length >= 10) {
-            console.log('country >10', country);
             const myStack = new Stack({
                 delay: 1000,
                 dir1: 'down',
